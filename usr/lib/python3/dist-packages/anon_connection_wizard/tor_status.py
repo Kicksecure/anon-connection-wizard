64589c61463e79681424b58ec1679927fbbc4d2d
irykoon <irykoon@gmail.com>
2018-01-13 18:21:07 +0000

    Simplify tor_status.py
    Since DisableNetwork 0 will always be along with the new generated anon_connection_wizard.torrc file. We can safely simplify tor_status.py

--- usr/lib/python3/dist-packages/anon_connection_wizard/tor_status.py
+++ usr/lib/python3/dist-packages/anon_connection_wizard/tor_status.py
@@ -5,7 +5,8 @@ import os, time
 from subprocess import call
 from anon_connection_wizard import repair_torrc
 
-anon_connection_wizard_torrc_path = '/usr/local/etc/torrc.d/40'
+anon_connection_wizard_torrc_path = '/usr/local/etc/torrc.d/40_anon_connection_wizard.torrc'
+
 def tor_status():
     if not os.path.exists(anon_connection_wizard_torrc_path):
         return 'no_torrc'
@@ -33,65 +34,22 @@ This makes sense because when we call set_enabled() or set_disabled() we really 
 rather than receive a 'no_torrc' or 'bad_torrc' complain, which is not helpful for users.
 '''
 def set_enabled():
-    repair_torrc.repair_torrc()  # This gurantees a good torrc
+    command = 'systemctl --no-pager restart tor@default'
+    tor_status = call(command, shell=True)
 
-    fh = open(anon_connection_wizard_torrc_path,'r')
-    lines = fh.readlines()
-    fh.close()
-
-    for line in lines:
-        if line.strip() == 'DisableNetwork 0':
-
-            command = 'systemctl --no-pager restart tor@default'
-            tor_status = call(command, shell=True)
-
-            if tor_status != 0:
-                return 'cannot_connect'
-
-            command = 'systemctl --no-pager status tor@default'
-            tor_status = call(command, shell=True)
-
-            if tor_status != 0:
-                return 'cannot_connect'
-
-            return 'tor_already_enabled'
-
-        elif line.strip() == '#DisableNetwork 0':
-
-            for i, line in enumerate(fileinput.input(anon_connection_wizard_torrc_path, inplace=1)):
-                sys.stdout.write(line.replace('#DisableNetwork 0', 'DisableNetwork 0'))
-
-            command = 'systemctl --no-pager restart tor@default'
-            tor_status = call(command, shell=True)
-
-            if tor_status != 0:
-                return 'cannot_connect'
-
-            command = 'systemctl --no-pager status tor@default'
-            tor_status = call(command, shell=True)
-
-            if tor_status != 0:
-                return 'cannot_connect'
-
-            return 'tor_enabled'
-
-    return 'missing_disablenetwork_line'
+    if tor_status != 0:
+        return 'cannot_connect'
+    
+    command = 'systemctl --no-pager status tor@default'
+    tor_status = call(command, shell=True)
+    
+    if tor_status != 0:
+        return 'cannot_connect'
+    
+    return 'tor_already_enabled'
 
 def set_disabled():
-    repair_torrc.repair_torrc()  # This gurantees a good torrc    
-    
-    fh = open(anon_connection_wizard_torrc_path,'r')
-    lines = fh.readlines()
-    fh.close()
+    command = 'systemctl --no-pager stop tor@default'
+    call(command, shell=True)
 
-    for line in lines:
-        if line.strip() == '#DisableNetwork 0':
-            return 'tor_already_disabled'
-        elif line.strip() == 'DisableNetwork 0':
-            for i, line in enumerate(fileinput.input(anon_connection_wizard_torrc_path, inplace=1)):
-                sys.stdout.write(line.replace('DisableNetwork 0', '#DisableNetwork 0'))
-
-            command = 'systemctl --no-pager stop tor@default'
-            call(command, shell=True)
-
-            return 'tor_disabled'
+    return 'tor_disabled'
