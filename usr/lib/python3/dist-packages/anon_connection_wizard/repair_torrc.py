b8a34c0c1c184a7005ebadc921d9404621d66877
irykoon <irykoon@gmail.com>
2018-01-15 17:19:34 +0000

    Make repair torrc not whonix-specific

--- usr/lib/python3/dist-packages/anon_connection_wizard/repair_torrc.py
+++ usr/lib/python3/dist-packages/anon_connection_wizard/repair_torrc.py
@@ -4,14 +4,37 @@ import fileinput, os, shutil
 
 
 '''repair_torrc() function will be called when we want to gurantee the existence of:
-1. /etc/tor/torrc file
+1. /etc/torrc.d/
+2. /etc/tor/torrc
+3. "%include /etc/torrc.d" line in /etc/tor/torrc file
+
 '''
 
 def repair_torrc():
     repair_torrc_d()
 
+    if not os.path.exists('/etc/tor/torrc'):
+        with open('/etc/tor/torrc', "w+") as f:
+            f.write("%include /etc/torrc.d")
+    else:
+        with open('/etc/tor/torrc', "r") as f:
+            lines = f.readlines()
+            f.close()
 
-'''repair_torrc_d() will gurantee the existence of /etc/torrc.d and /usr/local/etc/torrc.d/
+        torrcd_line_exists = False
+        for line in lines:
+            str = line.strip()
+            if (str == '%include /etc/torrc.d'):
+                torrcd_line_exists = True
+
+        if not torrcd_line_exists:
+            with open('/etc/tor/torrc', "a") as f:
+                f.write("%include /etc/torrc.d\n")
+
+
+'''repair_torrc_d() will gurantee the existence of /etc/torrc.d
+and if anon-connection-wizard is in Whonix,
+then also gurantee the existence of /usr/local/etc/torrc.d/
 '''
 def repair_torrc_d():
     if not os.path.exists('/etc/torrc.d/'):
