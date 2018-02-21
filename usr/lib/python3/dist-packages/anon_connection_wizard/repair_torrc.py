a35b6ae20caad69c5fd9507b2adb89a4cc29fdd3
irykoon <irykoon@gmail.com>
2018-02-21 23:18:49 +0000

    /etc/torrc.d -> /etc/torrc.d/95_whonix.conf
    We should avoid any `%include directory` for now before this feature is available in Tor stable: https://trac.torproject.org/projects/tor/ticket/25140#ticket

--- usr/lib/python3/dist-packages/anon_connection_wizard/repair_torrc.py
+++ usr/lib/python3/dist-packages/anon_connection_wizard/repair_torrc.py
@@ -3,9 +3,9 @@
 import fileinput, os, shutil
 
 '''repair_torrc() function will be called when we want to gurantee the existence of:
-1. /etc/torrc.d/
+1. /etc/torrc.d/95_whonix.conf
 2. /etc/tor/torrc
-3. "%include /etc/torrc.d" line in /etc/tor/torrc file
+3. "%include /etc/torrc.d/95_whonix.conf" line in /etc/tor/torrc file
 '''
 
 if os.path.exists('/usr/share/anon-gw-base-files/gateway'):
@@ -18,7 +18,7 @@ def repair_torrc():
 
     if not os.path.exists('/etc/tor/torrc'):
         with open('/etc/tor/torrc', "w+") as f:
-            f.write("%include /etc/torrc.d")
+            f.write("%include /etc/torrc.d/95_whonix.conf")
             f.write('\n')
     else:
         with open('/etc/tor/torrc', "r") as f:
@@ -28,21 +28,21 @@ def repair_torrc():
         torrcd_line_exists = False
         for line in lines:
             str = line.strip()
-            if (str == '%include /etc/torrc.d'):
+            if (str == '%include /etc/torrc.d/95_whonix.conf'):
                 torrcd_line_exists = True
 
         if not torrcd_line_exists:
             with open('/etc/tor/torrc', "a") as f:
-                f.write("%include /etc/torrc.d\n")
+                f.write("%include /etc/torrc.d/95_whonix.conf\n")
                 f.write('\n')
 
 
-'''repair_torrc_d() will gurantee the existence of /etc/torrc.d
+'''repair_torrc_d() will gurantee the existence of /etc/torrc.d/95_whonix.conf
 and if anon-connection-wizard is in Whonix,
-then also gurantee the existence of /usr/local/etc/torrc.d/
+then also gurantee the existence of /usr/local/etc/torrc.d/95_whonix.conf
 '''
 def repair_torrc_d():
-    if not os.path.exists('/etc/torrc.d/'):
-        os.makedirs('/etc/torrc.d/')
-    if whonix and not os.path.exists('/usr/local/etc/torrc.d/'):
-        os.makedirs('/usr/local/etc/torrc.d/')
+    if not os.path.exists('/etc/torrc.d/95_whonix.conf'):
+        os.makedirs('/etc/torrc.d/95_whonix.conf')
+    if whonix and not os.path.exists('/usr/local/etc/torrc.d/95_whonix.conf'):
+        os.makedirs('/usr/local/etc/torrc.d/95_whonix.conf')
