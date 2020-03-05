c9c6154890a740b6780658e364fc0fa2b783fde4
Patrick Schleizer <adrelanos@riseup.net>
2020-03-05 08:14:56 -0500

    use /usr/lib/anon-gw-anonymizer-config/tor-config-sane instead to repair Tor config
    instead of replicating that logic here
    
    implemented for Whonix only
    
    not implemented for non-Whonix
    
    https://forums.whonix.org/t/configuring-onion-service/9042/36

--- usr/lib/python3/dist-packages/anon_connection_wizard/repair_torrc.py
+++ usr/lib/python3/dist-packages/anon_connection_wizard/repair_torrc.py
@@ -4,11 +4,7 @@
 ## See the file COPYING for copying conditions.
 
 import fileinput, os, shutil
-
-'''repair_torrc() function will be called when we want to guarantee the existence of:
-1. "%include /etc/torrc.d/95_whonix.conf" line in /etc/tor/torrc file
-2. "%include /usr/local/etc/torrc.d/" line in /etc/torrc.d/95_whonix.conf file
-'''
+from subprocess import check_output, STDOUT, call, Popen, PIPE
 
 if os.path.exists('/usr/share/anon-gw-base-files/gateway'):
     whonix=True
@@ -16,33 +12,14 @@ else:
     whonix=False
 
 def repair_torrc():
-    if not os.path.exists('/etc/torrc.d/'):
-        os.makedirs('/etc/torrc.d/')
+   if not whonix:
+      ## Not implemented for non-Whonix yet.
+      return
 
-    if whonix and not os.path.exists('/usr/local/etc/torrc.d/'):
-        os.makedirs('/usr/local/etc/torrc.d/')
-
-    if not os.path.exists('/etc/torrc.d/95_whonix.conf'):
-        with open('/etc/torrc.d/95_whonix.conf', "w+") as f:
-            f.write("%include /usr/local/etc/torrc.d/")
-            f.write('\n')
-
-    if not os.path.exists('/etc/tor/torrc'):
-        with open('/etc/tor/torrc', "w+") as f:
-            f.write("%include /etc/torrc.d/")
-            f.write('\n')
-    else:
-        with open('/etc/tor/torrc', "r") as f:
-            lines = f.readlines()
-            f.close()
-
-        torrcd_line_exists = False
-        for line in lines:
-            str = line.strip()
-            if (str == '%include /etc/torrc.d/'):
-                torrcd_line_exists = True
-
-        if not torrcd_line_exists:
-            with open('/etc/tor/torrc', "a") as f:
-                f.write("%include /etc/torrc.d/")
-                f.write('\n')
+   try:
+      command = ['/usr/lib/anon-gw-anonymizer-config/tor-config-sane']
+      p = Popen(command, stdout=PIPE, stderr=PIPE)
+      stdout, stderr = p.communicate()
+   except:
+      error_msg = "tor-config-sane unexpected error: " + str(sys.exc_info()[0])
+      print(error_msg)
