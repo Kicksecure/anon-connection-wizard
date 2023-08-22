7310096c47f8f5f8495fd0dc2cf0ffda8e47a7d5
Patrick Schleizer <adrelanos@whonix.org>
2023-08-22 17:33:26 -0400

    fix path

--- usr/lib/python3/dist-packages/anon_connection_wizard/edit_etc_resolv_conf.py
+++ usr/lib/python3/dist-packages/anon_connection_wizard/edit_etc_resolv_conf.py
@@ -17,7 +17,7 @@ def edit_etc_resolv_conf_add():
       return
 
    try:
-      command = ['/usr/lib/anon-gw-anonymizer-config/edit-etc-resolv-conf']
+      command = ['/usr/libexec/anon-gw-anonymizer-config/edit-etc-resolv-conf']
       p = Popen(command, stdout=PIPE, stderr=PIPE)
       stdout, stderr = p.communicate()
    except BaseException:
@@ -30,7 +30,7 @@ def edit_etc_resolv_conf_remove():
       return
 
    try:
-      command = ['/usr/lib/anon-gw-anonymizer-config/edit-etc-resolv-conf', 'remove']
+      command = ['/usr/libexec/anon-gw-anonymizer-config/edit-etc-resolv-conf', 'remove']
       p = Popen(command, stdout=PIPE, stderr=PIPE)
       stdout, stderr = p.communicate()
    except BaseException:
